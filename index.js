1589ab280e08d61abf3899e602be83fcaa1cb855
jesus <jrosalesavendano@gmail.com>
2020-12-10 22:11:07 -0600

    express

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("Hola Mundo!");
+const express = require('express');
+
+//crear servidor express
+
+const app = express();
+
+//Rutas
+app.get('/', (req,res) =>{
+  console.log('Se recibio la petición en /');
+  res.json({
+    ok:true,
+    msg:"Recibido"
+  });
+}
+);
+
+//configurar el puerto
+app.listen(4000, ()=>{
+console.log(`Backend corriendo en el puerto ${4000}`);
+})
