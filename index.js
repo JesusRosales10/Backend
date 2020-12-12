57d103c66be5e207dd60f86d3c289022e6c5fc42
jesus <jrosalesavendano@gmail.com>
2020-12-11 20:01:40 -0600

    Rutas de usuario

--- index.js
+++ index.js
@@ -1,26 +1,14 @@
 const express = require('express');
 require('dotenv').config();
-
 //crear servidor express
-
 const app = express();
-
-
 
 //Acceso al directorio público
 app.use(express.static('public'));
 
 
 //Rutas
-/*app.get('/', (req,res) =>{
-
-  res.json({
-    ok:true,
-    msg:"Recibido"
-  });
-}
-);*/
-
+app.use('/api/auth',require('./routes/auth'));
 //configurar el puerto
 app.listen(process.env.PORT, ()=>{
 console.log(`Backend corriendo en el puerto ${process.env.PORT}`);
